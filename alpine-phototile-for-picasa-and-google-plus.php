f240a49f663d731c200412745f2d94b08a155e6b
theAlpinePress <theAlpinePress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-24 19:01:12 +0000

    Very small change
    git-svn-id: https://plugins.svn.wordpress.org/alpine-photo-tile-for-picasa-and-google-plus/trunk@672631 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- alpine-phototile-for-picasa-and-google-plus.php
+++ alpine-phototile-for-picasa-and-google-plus.php
@@ -3,7 +3,7 @@
 Plugin Name: Alpine PhotoTile for Picasa and Google Plus
 Plugin URI: http://thealpinepress.com/alpine-phototile-for-picasa-and-google-plus/
 Description: The Alpine PhotoTile for Picasa and Google Plus is capable of retrieving photos from a particular user or album. The photos can be linked to the your Picasa page, a specific URL, or to a Fancybox slideshow. Also, the Shortcode Generator makes it easy to insert the widget into posts without learning any of the code. This lightweight but powerful widget takes advantage of WordPress's built in JQuery scripts to create a sleek presentation that I hope you will like.
-Version: 1.2.3.1
+Version: 1.2.3.2
 Author: the Alpine Press
 Author URI: http://thealpinepress.com/
 License: GNU General Public License v3.0
@@ -72,7 +72,7 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
     wp_enqueue_script( 'jquery');
     wp_enqueue_script($bot->wmenujs);
     wp_enqueue_style($bot->acss);
-    add_action('admin_print_footer_scripts', 'APTFFbyTAP_menu_toggles');
+    add_action('admin_print_footer_scripts', 'APTFPICAbyTAP_menu_toggles');
     
     // Only admin can trigger two week cache cleaning by visiting widgets.php
     $disablecache = $bot->get_option( 'cache_disable' );
